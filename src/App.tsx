f4ba8a979ffd271e0b6ce5d2085a2a56eb6d3a41
Danielhu3 <danielhu3123@gmail.com>
2022-09-16 16:22:07 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './styles/global'
+import Header from './components/header';
+import {ThemeProvider} from 'styled-components'
+import light from './styles/themes/light';
 
 function App() {
   return (
+    <ThemeProvider theme={light}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <Header />
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Container} from './styles'
+const Header: React.FC = () =>{
+    return(
+        <Container>
+            Hello World
+        </Container>
+    )
+}
+
+export default Header
